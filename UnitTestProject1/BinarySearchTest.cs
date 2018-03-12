56e9c11b4ac4be24411af1723956912123a6f406
nekochanoide <motya202@gmail.com>
2018-03-12 21:43:51 +0500

    full program
    Тесты сделаны в тестовом проекте

---
+++ UnitTestProject1/BinarySearchTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication;
+
+namespace BinarySearchTests
+{
+    [TestClass]
+    public class BinarySearchTest
+    {
+        [TestMethod]
+        public void TestFiveNumbers()
+
+        {
+            //Тестирование поиска с пятью числами
+
+            int[] fiveNumbers = new[] { -5, -4, -3, 0 , 100};
+
+            var result = Program.BinarySearch(fiveNumbers, -5);
+
+            Assert.AreEqual(expected: 0, actual: result);
+        }
+
+        [TestMethod]
+        public void TestNegativeNumbers()
+
+        {
+            //Тестирование поиска в отрицательных числах
+
+            int[] negativeNumbers = new[] { -5, -4, -3, -2 };
+
+            var result = Program.BinarySearch(negativeNumbers, -3);
+
+            Assert.AreEqual(expected: 2, actual: result);
+        }
+
+        [TestMethod]
+        public void TestNonExistentElement()
+
+        {
+            //Тестирование поиска отсутствующего элемента
+
+            int[] negativeNumbers = new[] { -5, -4, -3, -2 };
+
+            var result = Program.BinarySearch(negativeNumbers, -1);
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestRepitingElement()
+
+        {
+            //Тестирование поиска с повторяющимися элементами
+
+            int[] negativeNumbers = new[] { -5, -4, -4, -4, -3, -1 };
+
+            var result = Program.BinarySearch(negativeNumbers, -4);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestEmptyArray()
+
+        {
+            //Тестирование поиска пустого массива
+
+            int[] noNumbers = new int[] { };
+
+            var result = Program.BinarySearch(noNumbers, -4);
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestBigArray()
+
+        {
+            //Тестирование поиска массива из 100001 чисел
+            Random rnd = new Random();
+
+            int[] aLotOfNumbers = new int[100001];
+
+            for (int i = 0; i<aLotOfNumbers.Length; i++)
+            aLotOfNumbers[i] = i+1;
+
+            var result = Program.BinarySearch(aLotOfNumbers, 123);
+
+            Assert.AreEqual(122, result);
+        }
+
+    }
+}
---
+++ binary search/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApplication
+
+{
+    public class Program
+    {
+        public static int BinarySearch(int[] array, int value)
+        {
+            if (array.Length == 0)
+                return -1;
+
+            int left = 0, right = array.Length - 1;
+            int selectedNumber = right / 2;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (value != array[selectedNumber])
+                {
+                    if (value < array[selectedNumber])
+                    {
+                        if (selectedNumber == 0)
+                            return -1;
+
+                        right = selectedNumber - 1;
+                        selectedNumber = right / 2;
+                    }
+                    else
+                    {
+                        if (selectedNumber == 0)
+                            return selectedNumber + 1;
+                        if (selectedNumber == array.Length - 1)
+                            return -1;
+
+                        left = selectedNumber + 1;
+                        selectedNumber = (right + left) / 2;
+                    }
+                }
+                else break;
+            }
+            return selectedNumber;
+            //код поиска значения value в массиве arrays
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
